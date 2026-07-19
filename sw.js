a8167500ed3af9897af393d747ce19ed97751b2f
socialgraph <socialgraph@yandex.ru>
2026-07-19 19:01:40 +0300

    add manifest

--- sw.js
+++ sw.js
@@ -1,8 +1,4 @@
-// sw.js
-
 const GHPATH = '/wh-cut';
-
-// Префикс для кэша (меняйте при обновлении файлов)
 const APP_PREFIX = 'wh-cut_';
 const VERSION = '1.0.0';
 
@@ -14,37 +10,48 @@ const URLS = [
     `${GHPATH}/script.js`
 ];
 
+const CACHE_NAME = APP_PREFIX + VERSION
+
 // Установка Service Worker — кэшируем файлы
-self.addEventListener('install', function(e) {
+self.addEventListener('install', function (e) {
   e.waitUntil(
-    caches.open(APP_PREFIX + VERSION).then(function(cache) {
-      return cache.addAll(URLS);
+    caches.open(CACHE_NAME).then(function (cache) {
+      console.log('Installing cache : ' + CACHE_NAME);
+      return cache.addAll(URLS)
     })
-  );
-});
+  )
+})
 
 // Перехват запросов — отдаем из кэша, если есть
-self.addEventListener('fetch', function(e) {
+self.addEventListener('fetch', function (e) {
+  console.log('Fetch request : ' + e.request.url);
   e.respondWith(
-    caches.match(e.request).then(function(response) {
-      return response || fetch(e.request);
+    caches.match(e.request).then(function (request) {
+      if (request) {
+        console.log('Responding with cache : ' + e.request.url);
+        return request
+      } else {
+        console.log('File is not cached, fetching : ' + e.request.url);
+        return fetch(e.request)
+      }
     })
-  );
-});
+  )
+})
 
 // Активация — удаляем старые кэши
-self.addEventListener('activate', function(e) {
+self.addEventListener('activate', function (e) {
   e.waitUntil(
-    caches.keys().then(function(keyList) {
-      var cacheWhitelist = keyList.filter(function(key) {
-        return key.indexOf(APP_PREFIX) === 0;
-      });
-      cacheWhitelist.push(APP_PREFIX + VERSION);
-      return Promise.all(keyList.map(function(key, i) {
+    caches.keys().then(function (keyList) {
+      var cacheWhitelist = keyList.filter(function (key) {
+        return key.indexOf(APP_PREFIX)
+      })
+      cacheWhitelist.push(CACHE_NAME);
+      return Promise.all(keyList.map(function (key, i) {
         if (cacheWhitelist.indexOf(key) === -1) {
-          return caches.delete(keyList[i]);
+          console.log('Deleting cache : ' + keyList[i] );
+          return caches.delete(keyList[i])
         }
-      }));
+      }))
     })
-  );
-});
+  )
+})
